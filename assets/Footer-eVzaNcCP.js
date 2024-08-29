aaa0b64a32c1662f1e9bd356aa1467a1420444bb
derenik.gabrielyan <derenik.gabrielyan@nork.am>
2024-08-29 16:57:55 +0400

    Updates

---
+++ assets/Footer-eVzaNcCP.js
@@ -0,0 +1,41 @@
+import{r as c,j as e}from"./index-CQYXxCDM.js";/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=s=>s.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),n=(...s)=>s.filter((a,t,r)=>!!a&&r.indexOf(a)===t).join(" ");/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var f={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=c.forwardRef(({color:s="currentColor",size:a=24,strokeWidth:t=2,absoluteStrokeWidth:r,className:i="",children:l,iconNode:m,...d},x)=>c.createElement("svg",{ref:x,...f,width:a,height:a,stroke:s,strokeWidth:r?Number(t)*24/Number(a):t,className:n("lucide",i),...d},[...m.map(([h,u])=>c.createElement(h,u)),...Array.isArray(l)?l:[l]]));/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=(s,a)=>{const t=c.forwardRef(({className:r,...i},l)=>c.createElement(p,{ref:l,iconNode:a,className:n(`lucide-${g(s)}`,r),...i}));return t.displayName=`${s}`,t};/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=o("Facebook",[["path",{d:"M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z",key:"1jg4f8"}]]);/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=o("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]]);/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=o("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=o("Phone",[["path",{d:"M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z",key:"foiqr5"}]]),v=()=>e.jsxs("footer",{className:"   bg-gray-100",children:[e.jsx("div",{className:"container h-32 top-0 max-w-full grid grid-cols-1 md:grid-cols-2 gap-8 bg-gradient-to-b from-gray-400 to-gray-100"}),e.jsx("div",{className:" max-w-7xl mx-auto ",children:e.jsx("section",{className:"mx-12 w-auto h-auto bg-gray-100",children:e.jsxs("div",{className:"container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 mb-10",children:[e.jsxs("div",{children:[e.jsx("h2",{className:"text-2xl  mb-4 text-blue-900 font-bold",children:"Կոնտակտային տվյալներ"}),e.jsxs("ul",{className:"space-y-4",children:[e.jsxs("li",{className:"flex items-center text-lg font-medium text-black",children:[e.jsx(N,{className:"mr-2"}),"Հասցե՝ Երևան Կարապետ Ուլնեցու փող., 68 շենք"]}),e.jsxs("li",{className:"flex items-center text-lg font-medium text-black",children:[e.jsx(j,{className:"mr-2"}),"Էլ. փոստ․ info@nilsr.am"]}),e.jsxs("li",{className:"flex items-center text-lg font-medium text-black",children:[e.jsx(w,{className:"mr-2"}),"Հեռախոսահամար․ (010) 20 83 44"]}),e.jsxs("li",{className:"flex items-center text-lg font-medium text-blue-950",children:[e.jsx(b,{className:"mr-2"}),e.jsx("a",{href:"https://www.facebook.com/NationalInstituteofLabourandSocialResearch/",target:"_blank",className:"underline",children:" Ֆեյսբուքյան էջ"})]})]})]}),e.jsx("div",{className:"rounded-lg overflow-hidden",children:e.jsx("iframe",{className:" w-full h-full pb-10 overflow-hidden",src:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d97529.37015091018!2d44.539196!3d40.191424!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406aa2d30b904659%3A0x75ad4a7583ca0674!2s68%20Karapet%20Ulnetsi%20St%2C%20Yerevan%2C%20Armenia!5e0!3m2!1sen!2sus!4v1724064392408!5m2!1sen!2sus",loading:"lazy",children:"."})})]})})}),e.jsxs("div",{className:" p-3 text-center   bg-blue-900  flex justify-center items-center",children:[e.jsx("span",{className:"mr-2  text-black",children:"© 2023 Copyright: "}),e.jsx("p",{className:"copyright text-white",children:" National Institute of Labour and Social Research. All rights reserved."})]})]});export{v as default};
---
+++ assets/Lessonlogo-Cvn1DD_w.js
@@ -0,0 +1,1 @@
+const s="/assets/Lessonlogo-DZwejZ6O.jpg";export{s as i};
